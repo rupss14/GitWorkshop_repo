1de1abcea11a76c6bfdbf85715b1fb01f79dbb33
rupali_tripathy <tripathy_rupali@gmail.com>
2025-03-16 11:36:23 +0530

    changes made by dev-2

--- dev-1.py
+++ dev-1.py
@@ -1,2 +1,3 @@
 print("step-1: file created in dev1 to be fetched by dev-2")
 print("step-2: changes made in dev-1")
+print("step-3: changes made in dev-2")
---
+++ dev-2.py
@@ -0,0 +1,1 @@
+print("file created by dev-2 to be fetched by dev-1")
